483c4a082f5d518aedf683ca2992ef3887d360c3
root <003581@qipai.com>
2019-12-25 15:39:12 +0800

    修改

--- src/main/java/com/szb/webfluxssedemo/GenericEvent.java
+++ src/main/java/com/szb/webfluxssedemo/GenericEvent.java
@@ -1,9 +1,11 @@
 package com.szb.webfluxssedemo;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.context.ApplicationEvent;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class GenericEvent<T> extends ApplicationEvent {
 
     public GenericEvent(T source) {
